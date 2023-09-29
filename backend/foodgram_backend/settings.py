5e5b332623f0ebf959d7cec3cb4ad39552b83441
Rybkin23 <aleksandr1040@yandex.ru>
2023-09-29 10:18:46 +0400

    up postgresql

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -63,25 +63,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv(
+            'DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', 'foodgram'),
+        'USER': os.getenv('POSTGRES_USER', 'foodgram'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'foodgram'),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': os.getenv(
-                'DB_ENGINE', 'django.db.backends.postgresql'),
-            'NAME': os.getenv('DB_NAME', 'foodgram'),
-            'USER': os.getenv('POSTGRES_USER', 'foodgram'),
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'foodgram'),
-            'HOST': os.getenv('DB_HOST', ''),
-            'PORT': os.getenv('DB_PORT', 5432)
-        }
-    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
