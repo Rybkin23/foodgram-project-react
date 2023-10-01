e14548b04fcb1b8a3d40783e4533a68e21744b97
Rybkin23 <aleksandr1040@yandex.ru>
2023-10-01 06:24:35 +0400

    fix bags.

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -119,7 +119,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'backend.api.pagination.CustomPageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPageNumberPagination',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
