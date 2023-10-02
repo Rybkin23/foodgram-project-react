586045362af8a56724437094211ec4c6ab8a1715
Rybkin23 <aleksandr1040@yandex.ru>
2023-10-02 18:18:06 +0400

    fix bags.

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', default='default')
 
 DEBUG = os.getenv('DEBUG', default='True')
-print('DEBUG=', DEBUG)
+
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='[]').split(',')
 
 INSTALLED_APPS = [
